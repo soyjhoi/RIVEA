46b2b319d9e868ba2be05298750f98a41acd2346
soyjhoi <joyyan.nzx@gmail.com>
2022-09-06 10:04:15 +1200

    Initial Commit

---
+++ RIVEA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RIVEA
+//
+//  Created by Joy Yan on 6/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RIVEA/RIVEAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIVEAApp.swift
+//  RIVEA
+//
+//  Created by Joy Yan on 6/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RIVEAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
